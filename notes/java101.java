5deea0ebfffedcb8434292d9aa952bc8a9cfe532
Anatoliy Dimitrov <tollodim@gmail.com>
2017-06-27 17:26:49 +0300

    Java notes

---
+++ notes/java101.java
@@ -0,0 +1,8 @@
+// All objects in Java have a toString() method, inherited from the Object class.
+
+// The method equals() tests strings for equality. In contrast, == tests for objects identity, i.e. whether the two tested arguments are the same.
+
+//Example with the string method contains()
+String log = "There is an emergency";
+if(log.contains("emergency")) pageSomeone();
+
